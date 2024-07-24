f33b71e0cfb8a46409588b97803b6703f3d8966d
francobko <franco.condor@urp.edu.pe>
2024-07-23 19:10:43 -0500

    feat: add movie data to /movies route

--- main.py
+++ main.py
@@ -3,13 +3,25 @@ from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
+lista_movies = [
+    {
+        "id": 1,
+        "title": "Avatar",
+        "director": "James Cameron",
+        "overview": "A paraplegic marine dispatched to the moon Pandora on a unique mission becomes torn between following his orders and protecting the world he feels is his home.",
+        "year": 2009,
+        "rating": 7.8,
+        "category": "Action"
+    }
+]
+
 @app.get("/", tags=["Home"])
 def home():
     return "Hello World"
 
 @app.get("/movies", tags=["Home"])
 def movies():
-    return {"Hello": "World"}
+    return lista_movies
 
 @app.get("/html", tags=["Home"])
 def html():
