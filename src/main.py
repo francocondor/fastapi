9aecefb6b0c28b1115b4455714caf46612154a10
francobko <franco.condor@urp.edu.pe>
2024-07-26 10:05:41 -0500

    feat: Update get_users and get_customers endpoints to use Annotated for common_params dependency

--- src/main.py
+++ src/main.py
@@ -31,17 +31,22 @@ def html():
 def home(request: Request):
     return templates.TemplateResponse('index.html', {'message': 'Welcome', 'request': request})
 
-def common_params(start_date: str, end_date: str):
-    return {"start_date": start_date, "end_date": end_date}
-CommonDep = Annotated[dict, Depends(common_params)]
+# def common_params(start_date: str, end_date: str):
+#     return {"start_date": start_date, "end_date": end_date}
+# CommonDep = Annotated[dict, Depends(common_params)]
+
+class CommonDep:
+    def __init__(self, start_date: str, end_date: str):
+        self.start_date = start_date
+        self.end_date = end_date
 
 @app.get('/users', tags=["Users"])
-def get_users(commons: CommonDep):
-    return f"Users created between {commons['start_date']} and {commons['end_date']}"
+def get_users(commons: CommonDep = Depends(CommonDep)):
+    return f"Users created between {commons.start_date} and {commons.end_date}"
 
 @app.get('/customers', tags=["Customers"])
-def get_customers(commons: CommonDep):
-    return f"Customers created between {commons['start_date']} and {commons['end_date']}"
+def get_customers(commons: CommonDep = Depends(CommonDep)):
+    return f"Customers created between {commons.start_date} and {commons.end_date}"
 
 @app.get('/get_file', tags=["Files"])
 def get_file():
