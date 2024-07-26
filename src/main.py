12708c53051e429b0c05dc466cf3b0ce0d962641
francobko <franco.condor@urp.edu.pe>
2024-07-26 09:10:26 -0500

    feat: Add static files and templates to FastAPI app

--- src/main.py
+++ src/main.py
@@ -4,6 +4,10 @@ from fastapi.responses import HTMLResponse, PlainTextResponse, FileResponse, Res
 from src.routers.movie_router import movie_router
 from src.utils.http_error_handle import HTTPErrorHandler
 
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+import os
+
 app = FastAPI()
 
 # app.add_middleware(HTTPErrorHandler)
@@ -12,13 +16,19 @@ async def http_error_handler(request: Request, call_next)-> Response | JSONRespo
     print('Middleware is running!')
     return await call_next(request)
 
+static_path = os.path.join(os.path.dirname(__file__), 'static')
+templates_path = os.path.join(os.path.dirname(__file__), 'templates')
+
+app.mount('/static', StaticFiles(directory=static_path), name='static')
+templates = Jinja2Templates(directory=templates_path)
+
 @app.get("/html", tags=["HTML"])
 def html():
     return HTMLResponse('<h1>Hello World</h1>')
 
 @app.get("/", tags=["Home"])
-def home():
-    return PlainTextResponse(content='Home', status_code=200)
+def home(request: Request):
+    return templates.TemplateResponse('index.html', {'message': 'Welcome', 'request': request})
 
 @app.get('/get_file', tags=["Files"])
 def get_file():
